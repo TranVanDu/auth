60b64fde2924c28b54fdb691fb87f3352b5c0f44
TranVanDu <du.tv08@gmail.com>
2020-07-23 13:05:36 +0700

    add_config

--- client/src/config/index.js
+++ client/src/config/index.js
@@ -1,3 +1,3 @@
 export const API_URL = "/api";
-export const BASE_URL = "https://auth-chat.herokuapp.com";
-//export const BASE_URL = "http://localhost:5000";
+//export const BASE_URL = "https://auth-chat.herokuapp.com";
+export const BASE_URL = "http://localhost:5000";
